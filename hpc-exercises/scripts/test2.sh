68d4e18111d8e5b9116a1e6311def79ac6e56a01
PI=vjaganna <student40@login8.hpc.binf.unibe.ch>
2024-11-12 17:04:21 +0100

    Add random number generation script

--- hpc-exercises/scripts/test2.sh
+++ hpc-exercises/scripts/test2.sh
@@ -1,4 +1,3 @@
-for i in {1..1000}; do echo 772 >>randomNumbers.txt; donesort -n randomNumbers.txt
-cat >test2.sh <<E0L
-for i in {1..1000}; do echo 992 >>randomNumbers.txt; done
+#!/bin/bash
+for i in {1..1000}; do echo 18667 >>randomNumbers.txt; done
 sort -n randomNumbers.txt
