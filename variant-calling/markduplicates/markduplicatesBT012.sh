f58555110d652dbf43d97657fd3f0a6a8e806c71
Anna M. <student40@login8.hpc.binf.unibe.ch>
2024-11-15 09:14:00 +0100

    /15th morning

---
+++ variant-calling/markduplicates/markduplicatesBT012.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Slurm options
+#SBATCH --mail-user=noverifyan@gmail.com
+#SBATCH --mail-type=fail,end
+#SBATCH --job-name="markDuplicates"
+#SBATCH --chdir=.
+#SBATCH --time=3:00:00
+#SBATCH --mem=4G
+#SBATCH --partition=pcourseb
+#SBATCH --output=markDuplicates_%j.out
+#SBATCH --error=markDuplicates_%j.err
+
+
+module load GATK/4.2.6.1-GCCcore-10.3.0-Java-11
+module load Java/17.0.6
+
+gatk MarkDuplicates INPUT=BT012.sorted.bam OUTPUT=BT012.dedup.bam REMOVE_DUPLICATES=FALSE METRICS_FILE=BT012.marked_dup_metrics.txt
